00fc480d9f36fbdf10a5247c3398feb7e281fc44
Nikita Luparev <nikita.luparev@gmail.com>
2016-10-30 15:19:15 +0500

    added bootstrap

--- src/App.elm
+++ src/App.elm
@@ -1,87 +1,43 @@
 import Html exposing (..)
-import Html.App as App exposing (program)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
-import Counter
-import List
-import Random
+import Html.App exposing (program)
 
--- MODEL
-type alias AppModel = 
-  { counterModels : List Counter.Model }
+main = program
+  { init = init 
+  , view = view 
+  , update = update 
+  , subscriptions = subscriptions 
+  }
 
-initialModel : AppModel 
-initialModel = 
-  { counterModels = [{ id = 1, value = 2}, { id = 2, value = 2}] }
-  -- counterModels = [Counter.model]
-
-init : (AppModel, Cmd Msg)
+init : (Model, Cmd Msg) 
 init = 
-  (initialModel, Cmd.none)
+  (Model [], Cmd.none)
 
--- MESSAGES 
--- wraps to indicate that message belongs to particular component
--- This allows our app to route messages to the relevant components
-type Msg 
-  = CounterMsg Counter.Msg  
-  | AddCounter Int
-  | CreateCounter
-  | RemoveCounter
+type alias Task = 
+  { text : String 
+  , completed : Bool 
+  }
 
+type alias Model = 
+  { tasks : List Task }
 
--- UPDATE 
-update : Msg -> AppModel -> ( AppModel, Cmd Msg)
-update msg model =
-  case msg of 
-    AddCounter id ->
-      let 
-        counters = (Counter.Model id 0)::model.counterModels
-      in
-        ({ model | counterModels = counters }, Cmd.none)
+type Msg = DoSomething
 
-    CreateCounter ->
-      (model, Random.generate AddCounter (Random.int 1 100000))
-
-    RemoveCounter -> 
-      let 
-        shift     = List.drop 1
-        counters  = shift model.counterModels
-      in
-        ({ model | counterModels = counters }, Cmd.none)
-
-    CounterMsg subMsg -> 
-      let 
-         ( updatedCounterModels, counterCmd ) = Counter.update subMsg model.counterModels
-      in
-        ({ model | counterModels = updatedCounterModels }, Cmd.map CounterMsg counterCmd )
-
-
--- SUBSCRIPTIONS
-subscriptions : AppModel -> Sub Msg
+subscriptions : Model -> Sub Msg
 subscriptions model = 
   Sub.none
-  
 
--- VIEW 
-view : AppModel -> Html Msg
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =  
+  (model, Cmd.none)
+
+
+view : Model -> Html Msg 
 view model = 
-  let 
-    counters = 
-      List.map 
-        (\ counterModel -> App.map CounterMsg (Counter.view counterModel)) 
-        model.counterModels
-  in
-    div [] 
-      [ button [ onClick CreateCounter ] [ text "Add Counter" ]
-      , button [ onClick RemoveCounter ] [ text "Remove Counter" ]
-      , div [ class "counters-holder" ] counters
-      ] 
-    
+  div [] 
+    [
+      input [] []
+    ]
 
-main = 
-  program
-    { init = init
-    , view = view 
-    , update = update
-    , subscriptions = subscriptions
-    }
+
+
